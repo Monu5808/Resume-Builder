5ba2a74dfed534b10089a3917db6e003bc4445d7
Monu5808 <manvendra.singh5808@gmail.com>
2025-09-09 18:41:55 +0530

    Home Page Edits

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,21 +6,25 @@ import mainImage from '../Assets/home-img.png'
 const Home = () => {
   return (
     <div className='home-main'>
-      <div className='home-left'>
-          <h1>Your resume in three <span style={{color:"#38b2a9"}}>easy</span>  steps</h1>
-          <p>Resume builder tools that assemble well-formatted resume. Through a resume builder, you can create a professional-looking resume in a few easy steps. This resume builder offer different template options, so you can select the template that best fits your needs and style.</p>
+      <div className='inner-home'>
 
-          <ol>
-            <li>Select a template from our collection.</li>
-            <li>Build you resume using our easy to use resume builder.</li>
-            <li>Download your resume.</li>
-          </ol>
-      </div>
+          <div className='home-left'>
+              <h1 style={{textAlign:"center", fontSize:"60px", lineHeight:"65px", fontWeight:"600"}} >Your resume in three <span style={{color:"#38b2a9"}}>easy</span>  steps</h1>
+              <p style={{textAlign:"center", color:"#575757ff"}}>Resume builder tools that assemble well-formatted resume. Through a resume builder, you can create a professional-looking resume in a few easy steps. This resume builder offer different template options, so you can select the template that best fits your needs and style.</p>
 
-      <div className='home-right'>
-        <img src={mainImage} /> 
-        <button>Select Template</button>
-      </div>
+              <ol>
+                <li>Select a template from our collection.</li>
+                <li>Build you resume using our easy to use resume builder.</li>
+                <li>Download your resume.</li>
+              </ol>
+          </div>
+
+          <div className='home-right'>
+            <img src={mainImage} /> 
+            <button>Select Template</button>
+          </div>
+          
+        </div>
 
     </div>
   )
